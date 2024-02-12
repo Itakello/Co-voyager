97b9e4db4d260ae93c6426cc12a074082ddba840
Itakello <maxste000@gmail.com>
2024-02-12 17:22:25 +0100

    ok

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ voyager = Voyager(
     azure_login=get_azure_login(),
     curriculum_agent_mode="manual",
     critic_agent_mode="manual",
-    resume=True,
+    resume=False,
 )
 
 voyager.learn()
